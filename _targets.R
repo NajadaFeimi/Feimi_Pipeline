950f6b8a7382fef0da3c4272a0b80cc2f95d14d4
Najada Feimi <najada.feimi.001@student.uni.lu>
2023-01-13 21:54:42 +0100

    Pipeline commit

---
+++ _targets.R
@@ -0,0 +1,11 @@
+install.packages("targets")
+library(targets)
+source("functions.R")
+tar_option_set(packages = c("readr", "dplyr", "ggplot2", "MyPackage", "remotes"))
+tar_option_set(envir = getNamespace("MyPackage"))
+
+list(
+  tar_target(model, fit_model(diamonds)),
+  tar_target(plot_abline, plot_model(model)),
+  tar_target(plot_boxplot, plot_stats(diamonds))
+)
---
+++ functions.R
@@ -0,0 +1,12 @@
+fit_model <- function(data) {
+  MyPackage::fit_model()
+}
+
+plot_model <- function(model) {
+  MyPackage::plot_model()
+}
+
+plot_stats <-function(data)
+{
+  MyPackage::plot_stats()
+}
